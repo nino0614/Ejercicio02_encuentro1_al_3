39c3ffab2848c85e2083bf403990d8429c7fa3bc
Nino Alonzo <ninito140677@gmail.com>
2023-08-25 01:06:05 -0300

    Initial commit

---
+++ src/ejercicio02_encuentro1_al_3/Ejercicio02_encuentro1_al_3.java
@@ -0,0 +1,14 @@
+
+package ejercicio02_encuentro1_al_3;
+
+
+public class Ejercicio02_encuentro1_al_3 {
+
+    
+    public static void main(String[] args) {
+       
+        
+        System.out.println("Ejercicio02 con github");
+    }
+    
+}
